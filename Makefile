b55db87b23dd2bf54f3a0657d80cf36b1438e032
Andrii <youdronveduta@gmail.com>
2024-03-27 09:38:30 +0100

    --fix by eslint

--- Makefile
+++ Makefile
@@ -1,8 +1,11 @@
-install: #перовое клонирование репозитория
+install: #Перовое клонирование репозитория
 	npm ci
 
 brain-games: #Запуск игры
 	node bin/brain-games.js
 
-publish: #публикация пакета
+publish: #Публикация пакета
 	npm publish --dry-run
+
+lint:	#Проверка линтером
+	npx eslint .
--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,8 @@
 import readlineSync from 'readline-sync';
+
 const sayHello = () => {
-    var userName = readlineSync.question('May I have your name? ');
-    console.log('Hello, ' + userName + '!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
 };
 
-export default sayHello ;
+export default sayHello;
