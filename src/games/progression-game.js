0a4dbea9e1fb193b300fc4cafecef8cee4c9a635
Andrii <youdronveduta@gmail.com>
2024-03-28 22:19:07 +0100

    add src/games/progression-game.js

---
+++ src/games/progression-game.js
@@ -0,0 +1,29 @@
+import {
+  getNumber, printAnswer, generateArr, getAmswer, printResult, sayWelcome,
+} from '../index.js';
+
+const hideOneIndexOfArr = (arr, index) => {
+  const retultArr = [...arr];
+  retultArr[index] = '..';
+  return retultArr;
+};
+
+const progressionGame = () => {
+  const userName = sayWelcome();
+  console.log('What number is missing in the progression?');
+  for (let i = 0; i < 3; i += 1) {
+    const arrOperation = generateArr();
+    const indexOfArr = getNumber(0, arrOperation.length - 1);
+    const question = hideOneIndexOfArr(arrOperation, indexOfArr);
+    const userAnswer = getAmswer(question.join(' '));
+    const result = printAnswer(arrOperation[indexOfArr], userAnswer);
+    if (printResult(result, userName) === false) {
+      break;
+    }
+    if (i === 2) {
+      console.log(`Congratulations, ${userName}!`);
+    }
+  }
+};
+
+export default progressionGame;
