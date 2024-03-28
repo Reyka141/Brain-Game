90683dfb4e8e5a979565914aabaf533530db6a4a
Andrii <youdronveduta@gmail.com>
2024-03-28 22:48:26 +0100

    update src/games/calc-game.js

--- src/games/calc-game.js
+++ src/games/calc-game.js
@@ -1,39 +1,19 @@
-import readlineSync from 'readline-sync';
-import { getNumber, printAnswer, genArrOperation } from '../index.js';
+import {
+  getNumber, printAnswer, genArrOperation, getAnswer, printResult, sayWelcome, isCalc,
+} from '../index.js';
 
-const isCalc = (firstNum, secondNum, arrOperation, userAnswer) => {
-  let calc;
-  switch (arrOperation) {
-    case '+':
-      calc = firstNum + secondNum;
-      break;
-    case '-':
-      calc = firstNum - secondNum;
-      break;
-    case '*':
-      calc = firstNum * secondNum;
-      break;
-    default:
-      calc = 'error';
-  }
-  const answer = printAnswer(calc.toString(), userAnswer);
-  return answer;
-};
-
-const calcGame = (userName) => {
+const calcGame = () => {
+  const userName = sayWelcome();
+  console.log('What is the result of the expression?');
   const arrOperation = genArrOperation();
   for (let i = 0; i < 3; i += 1) {
     const firstNum = getNumber();
     const secondNum = getNumber();
     const question = (`${firstNum} ${arrOperation[i]} ${secondNum}`);
-    console.log(`Question: ${question}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-    const result = isCalc(firstNum, secondNum, arrOperation[i], userAnswer);
-    if (result === 'Correct!') {
-      console.log(result);
-    } else {
-      console.log(result);
-      console.log(`Let's try again, ${userName}!`);
+    const userAnswer = getAnswer(question);
+    const systemAnswer = isCalc(firstNum, secondNum, arrOperation[i]);
+    const result = printAnswer(systemAnswer, userAnswer);
+    if (printResult(result, userName) === false) {
       break;
     }
     if (i === 2) {
