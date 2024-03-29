31f4edabb036cd8482527878c5e046bcfb881c50
Andrii <youdronveduta@gmail.com>
2024-03-29 12:20:20 +0100

    update src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,7 @@ import readlineSync from 'readline-sync';
 
 const sayHello = () => {
   const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
   return userName;
 };
 
