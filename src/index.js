5f124dda3ea3b51cac29393f7fec15fa86bdb223
Andrii <youdronveduta@gmail.com>
2024-03-31 23:14:14 +0200

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import readlineSync from 'readline-sync';
 import sayHello from './cli.js';
 
-const getNumber = (minNum = 1, maxNum = 100) => {
+export const getNumber = (minNum = 1, maxNum = 100) => {
   const generateNumForGame = Math.floor(Math.random() * maxNum) + minNum;
   return generateNumForGame;
 };
 
-const printAnswer = (systemAnswer, answerUser) => {
+export const printAnswer = (systemAnswer, answerUser) => {
   let answer = '';
   if (systemAnswer === answerUser) {
     answer = 'Correct!';
@@ -16,21 +16,13 @@ const printAnswer = (systemAnswer, answerUser) => {
   return answer;
 };
 
-const genArrOperation = () => {
+export const genOperation = () => {
   const operations = ['+', '-', '*'];
-  const arr = [];
-  const temp = [];
-  while (arr.length < 3) {
-    const randNum = Math.floor(Math.random() * 3);
-    if (!temp.includes(randNum)) {
-      temp.push(randNum);
-      arr.push(operations[randNum]);
-    }
-  }
-  return arr;
+  const randomOperation = operations[Math.floor(Math.random() * 3)];
+  return randomOperation;
 };
 
-const hasNod = (firstNum, secondNum) => {
+export const hasNod = (firstNum, secondNum) => {
   let minNum;
   const arrOfDivider = [];
   if (firstNum < secondNum) {
@@ -47,25 +39,25 @@ const hasNod = (firstNum, secondNum) => {
   return maxOfDivider;
 };
 
-const generateArr = () => {
+export const generateArr = () => {
   const lengthOfArr = getNumber(5, 12);
   const progression = getNumber(2, 10);
   let startNum = getNumber(1, 50);
-  const arr = [startNum.toString()];
+  const arr = [startNum];
   for (let i = 1; i < lengthOfArr; i += 1) {
     startNum += progression;
-    arr.push(startNum.toString());
+    arr.push(startNum);
   }
   return arr;
 };
 
-const getAnswer = (question) => {
+export const getAnswer = (question) => {
   console.log(`Question: ${question}`);
   const userAnswer = readlineSync.question('Your answer: ');
   return userAnswer;
 };
 
-const printResult = (result, userName) => {
+export const printResult = (result, userName) => {
   if (result === 'Correct!') {
     console.log(result);
   } else {
@@ -76,13 +68,13 @@ const printResult = (result, userName) => {
   return true;
 };
 
-const sayWelcome = () => {
+export const sayWelcome = () => {
   console.log('Welcome to the Brain Games!');
   const userName = sayHello();
   return userName;
 };
 
-const isCalc = (firstNum, secondNum, arrOperation) => {
+export const isCalc = (firstNum, secondNum, arrOperation) => {
   let calc;
   switch (arrOperation) {
     case '+':
@@ -100,18 +92,18 @@ const isCalc = (firstNum, secondNum, arrOperation) => {
   return calc.toString();
 };
 
-const isEven = (systemNumber) => {
+export const isEven = (systemNumber) => {
   const systemAnswer = (systemNumber % 2 === 0) ? 'yes' : 'no';
   return systemAnswer;
 };
 
-const hideOneIndexOfArr = (arr, index) => {
+export const hideOneIndexOfArr = (arr, index) => {
   const retultArr = [...arr];
   retultArr[index] = '..';
   return retultArr;
 };
 
-const isPrime = (num) => {
+export const isPrime = (num) => {
   let temp = true;
   for (let i = 2; i < (num / 2); i += 1) {
     if (num % i === 0) {
@@ -123,7 +115,73 @@ const isPrime = (num) => {
   return result;
 };
 
-export {
-  getNumber, printAnswer, genArrOperation, hasNod, generateArr, getAnswer, printResult,
-  sayWelcome, isCalc, isEven, hideOneIndexOfArr, isPrime,
+export const startGame = (description, quest, systemAnswer) => {
+  const userName = sayWelcome();
+  console.log(description);
+  for (let i = 0; i < 3; i += 1) {
+    const question = quest();
+    const sysAnswer = systemAnswer(question);
+    const userAnswer = getAnswer(question);
+    const result = printAnswer(sysAnswer, userAnswer);
+    if (printResult(result, userName) === false) {
+      break;
+    }
+    if (i === 2) {
+      console.log(`Congratulations, ${userName}!`);
+    }
+  }
+};
+
+export const generateQuestionForProgressionGame = () => {
+  const arrOperation = generateArr();
+  const indexOfArr = getNumber(0, arrOperation.length - 1);
+  const question = hideOneIndexOfArr(arrOperation, indexOfArr);
+  return question.join(' ');
+};
+
+export const createSysAnswerProgressionGame = (question) => {
+  const arrFromquest = question.split(' ');
+  const indexHide = arrFromquest.indexOf('..');
+  let progression;
+  let result;
+  if (indexHide >= arrFromquest.length - 2) {
+    progression = Number(arrFromquest[indexHide - 1]) - Number(arrFromquest[indexHide - 2]);
+    result = Number(arrFromquest[indexHide - 1]) + progression;
+  } else {
+    progression = Number(arrFromquest[indexHide + 2]) - Number(arrFromquest[indexHide + 1]);
+    result = Number(arrFromquest[indexHide + 1]) - progression;
+  }
+
+  return result.toString();
+};
+
+export const genCalcQuestion = () => {
+  const firstNum = getNumber();
+  const secondNum = getNumber();
+  const currentOperation = genOperation();
+  const question = (`${firstNum} ${currentOperation} ${secondNum}`);
+  return question;
+};
+
+export const createSysAnswerForCalcGame = (question) => {
+  const arrFromquest = question.split(' ');
+  const firstNum = Number(arrFromquest[0]);
+  const secondNum = Number(arrFromquest[2]);
+  const result = isCalc(firstNum, secondNum, arrFromquest[1]);
+  return result;
+};
+
+export const generateQuestionForGcdGame = () => {
+  const firstNum = getNumber();
+  const secondNum = getNumber();
+  const question = (`${firstNum} ${secondNum}`);
+  return question;
+};
+
+export const createSysAnswerForGcdGame = (question) => {
+  const arrFromquest = question.split(' ');
+  const firstNum = Number(arrFromquest[0]);
+  const secondNum = Number(arrFromquest[1]);
+  const result = hasNod(firstNum, secondNum);
+  return result;
 };
