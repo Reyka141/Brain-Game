bbcbba9adeb56185709ab543233b6f2c03194634
Andrii <youdronveduta@gmail.com>
2024-03-31 23:16:13 +0200

    update src/games/prime-game.js

--- src/games/prime-game.js
+++ src/games/prime-game.js
@@ -1,22 +1,10 @@
 import {
-  getNumber, printAnswer, getAnswer, printResult, sayWelcome, isPrime,
+  getNumber, isPrime, startGame,
 } from '../index.js';
 
 const primeGame = () => {
-  const userName = sayWelcome();
-  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-  for (let i = 0; i < 3; i += 1) {
-    const question = getNumber(2, 97);
-    const userAnswer = getAnswer(question);
-    const systemAnswer = isPrime(question);
-    const result = printAnswer(systemAnswer, userAnswer);
-    if (printResult(result, userName) === false) {
-      break;
-    }
-    if (i === 2) {
-      console.log(`Congratulations, ${userName}!`);
-    }
-  }
+  const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+  startGame(description, getNumber, isPrime);
 };
 
 export default primeGame;
