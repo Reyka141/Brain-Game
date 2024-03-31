4c4d0c00044bc34b407543d41b25addeb353048a
Andrii <youdronveduta@gmail.com>
2024-03-31 23:14:45 +0200

    update src/game/calc-game.js

--- src/games/calc-game.js
+++ src/games/calc-game.js
@@ -1,25 +1,10 @@
 import {
-  getNumber, printAnswer, genArrOperation, getAnswer, printResult, sayWelcome, isCalc,
+  startGame, genCalcQuestion, createSysAnswerForCalcGame,
 } from '../index.js';
 
 const calcGame = () => {
-  const userName = sayWelcome();
-  console.log('What is the result of the expression?');
-  const arrOperation = genArrOperation();
-  for (let i = 0; i < 3; i += 1) {
-    const firstNum = getNumber();
-    const secondNum = getNumber();
-    const question = (`${firstNum} ${arrOperation[i]} ${secondNum}`);
-    const userAnswer = getAnswer(question);
-    const systemAnswer = isCalc(firstNum, secondNum, arrOperation[i]);
-    const result = printAnswer(systemAnswer, userAnswer);
-    if (printResult(result, userName) === false) {
-      break;
-    }
-    if (i === 2) {
-      console.log(`Congratulations, ${userName}!`);
-    }
-  }
+  const description = 'What is the result of the expression?';
+  startGame(description, genCalcQuestion, createSysAnswerForCalcGame);
 };
 
 export default calcGame;
