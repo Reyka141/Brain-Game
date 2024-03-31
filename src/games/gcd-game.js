651f6d5d0219ede87342acd202c9bc09b00052d7
Andrii <youdronveduta@gmail.com>
2024-03-31 23:15:45 +0200

    update src/games/gcd-game.js

--- src/games/gcd-game.js
+++ src/games/gcd-game.js
@@ -1,24 +1,10 @@
 import {
-  getNumber, printAnswer, getAnswer, printResult, sayWelcome, hasNod,
+  startGame, generateQuestionForGcdGame, createSysAnswerForGcdGame,
 } from '../index.js';
 
 const gcdGame = () => {
-  const userName = sayWelcome();
-  console.log('Find the greatest common divisor of given numbers.');
-  for (let i = 0; i < 3; i += 1) {
-    const firstNum = getNumber();
-    const secondNum = getNumber();
-    const question = (`${firstNum} ${secondNum}`);
-    const userAnswer = getAnswer(question);
-    const systemAnswer = hasNod(firstNum, secondNum);
-    const result = printAnswer(systemAnswer, userAnswer);
-    if (printResult(result, userName) === false) {
-      break;
-    }
-    if (i === 2) {
-      console.log(`Congratulations, ${userName}!`);
-    }
-  }
+  const description = 'Find the greatest common divisor of given numbers.';
+  startGame(description, generateQuestionForGcdGame, createSysAnswerForGcdGame);
 };
 
 export default gcdGame;
