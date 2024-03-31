e100ede029a27e86590e38da06da65d50909d67f
Andrii <youdronveduta@gmail.com>
2024-03-31 23:15:17 +0200

    update rc/games/even-game.js

--- src/games/even-game.js
+++ src/games/even-game.js
@@ -1,22 +1,10 @@
 import {
-  getNumber, printAnswer, getAnswer, printResult, sayWelcome, isEven,
+  getNumber, isEven, startGame,
 } from '../index.js';
 
 const evenGame = () => {
-  const userName = sayWelcome();
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  for (let i = 0; i < 3; i += 1) {
-    const question = getNumber();
-    const systemAnswer = isEven(question);
-    const userAnswer = getAnswer(question);
-    const result = printAnswer(systemAnswer, userAnswer);
-    if (printResult(result, userName) === false) {
-      break;
-    }
-    if (i === 2) {
-      console.log(`Congratulations, ${userName}!`);
-    }
-  }
+  const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+  startGame(description, getNumber, isEven);
 };
 
 export default evenGame;
