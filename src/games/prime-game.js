1a4b9ca78b0f638803b19430d74a016ef9ba4b76
Andrii <youdronveduta@gmail.com>
2024-04-01 12:03:42 +0200

    update src/games/prime-game.js

--- src/games/prime-game.js
+++ src/games/prime-game.js
@@ -1,10 +1,21 @@
-import {
-  getNumber, isPrime, startGame,
-} from '../index.js';
+import startGame from '../index.js';
+import getNumber from '../gen-num.js';
 
-const primeGame = () => {
+export const isPrime = (num) => {
+  let temp = true;
+  for (let i = 2; i < (num / 2); i += 1) {
+    if (num % i === 0) {
+      temp = false;
+      break;
+    }
+  }
+  const result = (temp === true) ? 'yes' : 'no';
+  return result;
+};
+
+const startPrimeGame = () => {
   const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
   startGame(description, getNumber, isPrime);
 };
 
-export default primeGame;
+export default startPrimeGame;
