eeacdb26e2f0c9e52421ca5ae42996cc865629c8
Andrii <youdronveduta@gmail.com>
2024-04-01 12:02:43 +0200

    update bin/brain-progression.js

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import progressionGame from '../src/games/progression-game.js';
+import startProgressionGame from '../src/games/progression-game.js';
 
-progressionGame();
+startProgressionGame();
