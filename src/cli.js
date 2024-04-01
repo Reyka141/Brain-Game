d48ce63c2a00572e75ddf369ba5e2dafb06ec195
Andrii <youdronveduta@gmail.com>
2024-04-01 16:01:10 +0200

    update src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,8 @@
 import readlineSync from 'readline-sync';
 
 const sayHello = () => {
-  console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-  return userName;
 };
 
 export default sayHello;
