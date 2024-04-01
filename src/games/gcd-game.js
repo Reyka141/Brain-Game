d26c2537eef76d68999e0bc2312fc1030111e731
Andrii <youdronveduta@gmail.com>
2024-04-01 12:03:28 +0200

    update src/games/gcd-game.js

--- src/games/gcd-game.js
+++ src/games/gcd-game.js
@@ -1,10 +1,41 @@
-import {
-  startGame, generateQuestionForGcdGame, createSysAnswerForGcdGame,
-} from '../index.js';
+import startGame from '../index.js';
+import getNumber from '../gen-num.js';
 
-const gcdGame = () => {
+const hasNod = (firstNum, secondNum) => {
+  let minNum;
+  const arrOfDivider = [];
+  if (firstNum < secondNum) {
+    minNum = firstNum;
+  } else {
+    minNum = secondNum;
+  }
+  for (let i = 1; i <= minNum; i += 1) {
+    if (firstNum % i === 0 && secondNum % i === 0) {
+      arrOfDivider.push(i);
+    }
+  }
+  const maxOfDivider = arrOfDivider.pop().toString();
+  return maxOfDivider;
+};
+
+const generateQuestionForGcdGame = () => {
+  const firstNum = getNumber();
+  const secondNum = getNumber();
+  const question = (`${firstNum} ${secondNum}`);
+  return question;
+};
+
+const createSysAnswerForGcdGame = (question) => {
+  const arrFromquest = question.split(' ');
+  const firstNum = Number(arrFromquest[0]);
+  const secondNum = Number(arrFromquest[1]);
+  const result = hasNod(firstNum, secondNum);
+  return result;
+};
+
+const startGcdGame = () => {
   const description = 'Find the greatest common divisor of given numbers.';
   startGame(description, generateQuestionForGcdGame, createSysAnswerForGcdGame);
 };
 
-export default gcdGame;
+export default startGcdGame;
