c12322ae6d738ac718f37ba2f064bbd97d2902e4
Andrii <youdronveduta@gmail.com>
2024-04-01 21:28:38 +0200

    update src/games/progression-game.js

--- src/games/progression-game.js
+++ src/games/progression-game.js
@@ -1,32 +1,21 @@
 import startGame from '../index.js';
 import getNumber from '../gen-num.js';
 
-const generateArr = () => {
+const genProgresQuestion = () => {
   const lengthOfArr = getNumber(5, 12);
   const progression = getNumber(2, 10);
   let startNum = getNumber(1, 50);
-  const arr = [startNum];
+  const arrOperation = [startNum];
   for (let i = 1; i < lengthOfArr; i += 1) {
     startNum += progression;
-    arr.push(startNum);
+    arrOperation.push(startNum);
   }
-  return arr;
+  const index = getNumber(0, arrOperation.length - 1);
+  arrOperation[index] = '..';
+  return arrOperation.join(' ');
 };
 
-const hideOneIndexOfArr = (arr, index) => {
-  const retultArr = [...arr];
-  retultArr[index] = '..';
-  return retultArr;
-};
-
-const generateQuestionForProgressionGame = () => {
-  const arrOperation = generateArr();
-  const indexOfArr = getNumber(0, arrOperation.length - 1);
-  const question = hideOneIndexOfArr(arrOperation, indexOfArr);
-  return question.join(' ');
-};
-
-const createSysAnswerProgressionGame = (question) => {
+const calcProgresNum = (question) => {
   const arrFromquest = question.split(' ');
   const indexHide = arrFromquest.indexOf('..');
   let progression;
@@ -44,7 +33,7 @@ const createSysAnswerProgressionGame = (question) => {
 
 const startProgressionGame = () => {
   const description = 'What number is missing in the progression?';
-  startGame(description, generateQuestionForProgressionGame, createSysAnswerProgressionGame);
+  startGame(description, genProgresQuestion, calcProgresNum);
 };
 
 export default startProgressionGame;
